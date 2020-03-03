5b14d5f8454702034257a1d8e9957773edd70dde
AleksandrKoltsov <41367097+AleksandrKoltsov@users.noreply.github.com>
2020-03-03 13:39:18 +0200

    todo build

---
+++ todo/precache-manifest.088cfc9222db48dc594b577965533546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50ae122a018bdffcc767fb8c7673cb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "539ac7226cec1491ccf7",
+    "url": "/static/css/main.e56f2c6b.chunk.css"
+  },
+  {
+    "revision": "3cb376f3e16d7c02850f",
+    "url": "/static/js/2.ce3b91ce.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ce3b91ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539ac7226cec1491ccf7",
+    "url": "/static/js/main.e43e0d62.chunk.js"
+  },
+  {
+    "revision": "39d47c611a20b26e8983",
+    "url": "/static/js/runtime-main.bcabb252.js"
+  }
+]);
